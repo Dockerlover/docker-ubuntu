571b0c56f8996da48c0da8ad902e9dec1c604c30
刘红 <liuhong1.happy@163.com>
2015-06-23 17:01:50 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,2 +1,1 @@
-HOST_IP=$(hostname --all-ip-addresses | awk '{print $1}')
-docker run -it -d --name ubuntu1 -p 22:22 -e HOST_IP=$HOST_IP docker-ubuntu
+docker run -it -d --name ubuntu1 -p 22:22  docker-ubuntu
